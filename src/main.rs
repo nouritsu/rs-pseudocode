da337d1a8d0f8e92ea777635819eec98edb8e860
Aneesh <aneesh1701@gmail.com>
2023-11-30 12:20:12 +0530

    switch parser to chumsky

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-use crate::parser::{PseudocodeParser, Rule};
-use pest::Parser;
-
-mod parser;
-
 fn main() {
-    let r = PseudocodeParser::parse(Rule::GroupingExpr, "(-1)");
-    println!("{:?}", r);
+    println!("Hello World!");
 }
