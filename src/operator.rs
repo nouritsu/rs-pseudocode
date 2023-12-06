c4118fd3429b6e71b0dc65ed1420cc8f452630aa
Aneesh <aneesh1701@gmail.com>
2023-12-06 12:02:18 +0530

    add comparison operators, impl FromStr for Operator

--- src/operator.rs
+++ src/operator.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::{fmt::Display, str::FromStr};
 
 #[derive(Debug)]
 pub enum Operator {
@@ -14,6 +14,14 @@ pub enum Operator {
     And,
     Or,
     Not,
+
+    // Comparison
+    Equals,
+    NotEquals,
+    GreaterThan,
+    LesserThan,
+    GreaterEqual,
+    LesserEqual,
 }
 
 impl Display for Operator {
@@ -25,9 +33,45 @@ impl Display for Operator {
             Operator::Division => write!(f, "{}", "/"),
             Operator::Quotient => write!(f, "{}", "DIV"),
             Operator::Modulo => write!(f, "{}", "MOD"),
+
             Operator::And => write!(f, "{}", "AND"),
             Operator::Or => write!(f, "{}", "OR"),
             Operator::Not => write!(f, "{}", "NOT"),
+
+            Operator::Equals => write!(f, "{}", "="),
+            Operator::NotEquals => write!(f, "{}", "<>"),
+            Operator::GreaterThan => write!(f, "{}", ">"),
+            Operator::LesserThan => write!(f, "{}", "<"),
+            Operator::GreaterEqual => write!(f, "{}", ">="),
+            Operator::LesserEqual => write!(f, "{}", "<="),
         }
+    }
+}
+
+impl FromStr for Operator {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "+" => Operator::Addition,
+            "-" => Operator::Subtraction,
+            "*" => Operator::Multiplication,
+            "/" => Operator::Division,
+            "DIV" => Operator::Quotient,
+            "MOD" => Operator::Modulo,
+
+            "AND" => Operator::And,
+            "OR" => Operator::Or,
+            "NOT" => Operator::Not,
+
+            "=" => Operator::Equals,
+            "<>" => Operator::NotEquals,
+            ">" => Operator::GreaterThan,
+            "<" => Operator::LesserThan,
+            ">=" => Operator::GreaterEqual,
+            "<=" => Operator::LesserEqual,
+
+            _ => return Err(()),
+        })
     }
 }
