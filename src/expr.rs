353b0bc5121634b0c63be4b853ba3a129ac1c5fa
Aneesh <aneesh1701@gmail.com>
2023-12-10 00:30:08 +0530

    change Display implementation to Debug

--- src/expr.rs
+++ src/expr.rs
@@ -1,8 +1,8 @@
 use crate::value::Value;
 use color_eyre::owo_colors::OwoColorize;
-use std::fmt::Display;
+use std::fmt::Debug;
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub enum Expr {
     Literal(Value),
     Variable(String),
@@ -28,27 +28,27 @@ pub enum Expr {
     LesserEqual(Box<Expr>, Box<Expr>),
 }
 
-impl Display for Expr {
+impl Debug for Expr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Expr::Literal(a) => write!(f, "{}", a.red()),
             Expr::Variable(a) => write!(f, "{}", a.bright_black()),
-            Expr::Negation(a) => write!(f, "({} {})", "-".purple(), a),
-            Expr::Not(a) => write!(f, "({} {})", "NOT".purple(), a),
-            Expr::Addition(a, b) => write!(f, "({} {} {})", a, "+".purple(), b),
-            Expr::Subtraction(a, b) => write!(f, "({} {} {})", a, "-".purple(), b),
-            Expr::Multiplication(a, b) => write!(f, "({} {} {})", a, "*".purple(), b),
-            Expr::Division(a, b) => write!(f, "({} {} {})", a, "/".purple(), b),
-            Expr::Quotient(a, b) => write!(f, "({} {} {})", a, "DIV".purple(), b),
-            Expr::Modulo(a, b) => write!(f, "({} {} {})", a, "MOD".purple(), b),
-            Expr::And(a, b) => write!(f, "({} {} {})", a, "AND".purple(), b),
-            Expr::Or(a, b) => write!(f, "({} {} {})", a, "OR".purple(), b),
-            Expr::Equals(a, b) => write!(f, "({} {} {})", a, "=".purple(), b),
-            Expr::NotEquals(a, b) => write!(f, "({} {} {})", a, "<>".purple(), b),
-            Expr::GreaterThan(a, b) => write!(f, "({} {} {})", a, ">".purple(), b),
-            Expr::LesserThan(a, b) => write!(f, "({} {} {})", a, "<".purple(), b),
-            Expr::GreaterEqual(a, b) => write!(f, "({} {} {})", a, ">=".purple(), b),
-            Expr::LesserEqual(a, b) => write!(f, "({} {} {})", a, "<=".purple(), b),
+            Expr::Negation(a) => write!(f, "({} {:?})", "-".purple(), a),
+            Expr::Not(a) => write!(f, "({} {:?})", "NOT".purple(), a),
+            Expr::Addition(a, b) => write!(f, "({:?} {} {:?})", a, "+".purple(), b),
+            Expr::Subtraction(a, b) => write!(f, "({:?} {} {:?})", a, "-".purple(), b),
+            Expr::Multiplication(a, b) => write!(f, "({:?} {} {:?})", a, "*".purple(), b),
+            Expr::Division(a, b) => write!(f, "({:?} {} {:?})", a, "/".purple(), b),
+            Expr::Quotient(a, b) => write!(f, "({:?} {} {:?})", a, "DIV".purple(), b),
+            Expr::Modulo(a, b) => write!(f, "({:?} {} {:?})", a, "MOD".purple(), b),
+            Expr::And(a, b) => write!(f, "({:?} {} {:?})", a, "AND".purple(), b),
+            Expr::Or(a, b) => write!(f, "({:?} {} {:?})", a, "OR".purple(), b),
+            Expr::Equals(a, b) => write!(f, "({:?} {} {:?})", a, "=".purple(), b),
+            Expr::NotEquals(a, b) => write!(f, "({:?} {} {:?})", a, "<>".purple(), b),
+            Expr::GreaterThan(a, b) => write!(f, "({:?} {} {:?})", a, ">".purple(), b),
+            Expr::LesserThan(a, b) => write!(f, "({:?} {} {:?})", a, "<".purple(), b),
+            Expr::GreaterEqual(a, b) => write!(f, "({:?} {} {:?})", a, ">=".purple(), b),
+            Expr::LesserEqual(a, b) => write!(f, "({:?} {} {:?})", a, "<=".purple(), b),
         }
     }
 }
