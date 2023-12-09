b308890ebb48afe3bc4868d1a391fe3d8d1efa6f
Aneesh <aneesh1701@gmail.com>
2023-12-10 00:29:52 +0530

    declare modules err, stmt and executor

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
+pub mod error;
 pub mod evaluator;
+pub mod executor;
 pub mod expr;
 pub mod parser;
+pub mod result;
+pub mod stmt;
 pub mod value;
 
 // Re-Exports
 pub use evaluator::eval;
+pub use executor::exec;
 pub use expr::Expr;
 pub use parser::parser;
+pub use stmt::Stmt;
 pub use value::Value;
